707450cb8d0a3a30749a40a7bb11945ffe3b6e27
Leo Qin <leozqin@gmail.com>
2024-09-10 20:07:20 -0700

    feat: return json response when json accept header is passed

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from typing import Annotated, Mapping, Sequence
 
 from fastapi import FastAPI, Form, UploadFile, status
 from fastapi.requests import Request
-from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi.responses import FileResponse, HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi_utils.tasks import repeat_every
@@ -17,6 +17,8 @@ from app.logging import HealthCheckFilter
 from app.models import Feed, HealthCheck
 from app.rss import PrecisRSS
 from app.storage.engine import load_storage_config
+
+JSON = "application/json"
 
 logger = getLogger("uvicorn.error")
 base_path = Path(__file__).parent
@@ -89,21 +91,23 @@ async def root(request: Request):
         return RedirectResponse("/onboarding/")
 
 
-@app.get("/about", response_class=HTMLResponse)
+@app.get("/about")
 async def about(
     request: Request, update_status: bool = False, update_exception: str = None
 ):
+    response = {
+        "settings": await bk.get_settings(),
+        "update_status": update_status,
+        "update_exception": update_exception,
+        **bk.about(),
+    }
 
-    return templates.TemplateResponse(
-        "about.html",
-        {
-            "request": request,
-            "settings": await bk.get_settings(),
-            "update_status": update_status,
-            "update_exception": update_exception,
-            **bk.about(),
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "about.html", {"request": request, **response}
+        )
 
 
 @app.get("/health", response_model=HealthCheck)
@@ -112,84 +116,96 @@ async def health_check(request: Request) -> HealthCheck:
     return bk.health_check()
 
 
-@app.get("/onboarding/", response_class=HTMLResponse)
+@app.get("/onboarding/")
 async def onboarding(request: Request):
+    response = {"settings": await bk.get_settings()}
 
-    return templates.TemplateResponse(
-        "onboarding.html",
-        {
-            "request": request,
-            "settings": await bk.get_settings(),
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "onboarding.html",
+            {"request": request, **response},
+        )
 
 
 @app.get("/list-entries/{feed_id}", response_class=HTMLResponse)
 async def list_entries_by_feed(
     feed_id: str, request: Request, refresh_requested: bool = False
 ):
+    response = {
+        "settings": await bk.get_settings(),
+        "entries": list(bk.list_entries(feed_id=feed_id)),
+        "feed": await bk.get_feed_config(id=feed_id),
+        "refresh_requested": refresh_requested,
+    }
 
-    return templates.TemplateResponse(
-        "entries.html",
-        {
-            "request": request,
-            "settings": await bk.get_settings(),
-            "entries": list(bk.list_entries(feed_id=feed_id)),
-            "feed": await bk.get_feed_config(id=feed_id),
-            "refresh_requested": refresh_requested,
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "entries.html",
+            {"request": request, **response},
+        )
 
 
 @app.get("/recent/", response_class=HTMLResponse)
 async def list_recent_feed_entries(request: Request, refresh_requested: bool = False):
+    response = {
+        "settings": await bk.get_settings(),
+        "entries": list(bk.list_entries(feed_id=None, recent=True)),
+        "refresh_requested": refresh_requested,
+        "recent": True,
+    }
 
-    return templates.TemplateResponse(
-        "entries.html",
-        {
-            "request": request,
-            "settings": await bk.get_settings(),
-            "entries": list(bk.list_entries(feed_id=None, recent=True)),
-            "refresh_requested": refresh_requested,
-            "recent": True,
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "entries.html",
+            {"request": request, **response},
+        )
 
 
 @app.get("/read/{feed_entry_id}", response_class=HTMLResponse)
 async def read(request: Request, feed_entry_id: str, redrive: bool = False):
+    response = {
+        "content": await bk.get_entry_content(
+            feed_entry_id=feed_entry_id, redrive=redrive
+        ),
+        "settings": await bk.get_settings(),
+    }
 
-    return templates.TemplateResponse(
-        "read.html",
-        {
-            "request": request,
-            "content": await bk.get_entry_content(
-                feed_entry_id=feed_entry_id, redrive=redrive
-            ),
-            "settings": await bk.get_settings(),
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "read.html",
+            {"request": request, **response},
+        )
 
 
 @app.get("/settings/", response_class=HTMLResponse)
 async def settings(
     request: Request, update_status: bool = False, update_exception: str = None
 ):
+    response = {
+        "themes": Themes._member_names_,
+        "content_handler_choices": await bk.list_content_handler_choices(),
+        "summarization_handler_choices": await bk.list_summarization_handler_choices(),
+        "notification_handler_choices": await bk.list_notification_handler_choices(),
+        "settings": await bk.get_settings(),
+        "notification": bk.get_handlers(),
+        "update_status": update_status,
+        "update_exception": update_exception,
+    }
 
-    return templates.TemplateResponse(
-        "settings.html",
-        {
-            "request": request,
-            "themes": Themes._member_names_,
-            "content_handler_choices": await bk.list_content_handler_choices(),
-            "summarization_handler_choices": await bk.list_summarization_handler_choices(),
-            "notification_handler_choices": await bk.list_notification_handler_choices(),
-            "settings": await bk.get_settings(),
-            "notification": bk.get_handlers(),
-            "update_status": update_status,
-            "update_exception": update_exception,
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "settings.html", {"request": request, **response}
+        )
 
 
 @app.get("/settings/{handler}", response_class=HTMLResponse)
@@ -199,18 +215,20 @@ async def handler_settings(
     update_status: bool = False,
     update_exception: str = None,
 ):
+    response = {
+        "handler": bk.get_handler_config(handler=handler),
+        "schema": bk.get_handler_schema(handler=handler),
+        "settings": await bk.get_settings(),
+        "update_status": update_status,
+        "update_exception": update_exception,
+    }
 
-    return templates.TemplateResponse(
-        "handler_config.html",
-        {
-            "request": request,
-            "handler": bk.get_handler_config(handler=handler),
-            "schema": bk.get_handler_schema(handler=handler),
-            "settings": await bk.get_settings(),
-            "update_status": update_status,
-            "update_exception": update_exception,
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "handler_config.html", {"request": request, **response}
+        )
 
 
 @app.post("/api/update_handler/", status_code=status.HTTP_200_OK)
@@ -410,31 +428,35 @@ async def feeds(
 async def feed_settings(
     request: Request, id: str, update_status: bool = False, update_exception: str = None
 ):
+    response = {
+        "settings": await bk.get_settings(),
+        "feed": await bk.get_feed_config(id=id),
+        "update_status": update_status,
+        "update_exception": update_exception,
+    }
 
-    return templates.TemplateResponse(
-        "feed_config.html",
-        {
-            "request": request,
-            "settings": await bk.get_settings(),
-            "feed": await bk.get_feed_config(id=id),
-            "update_status": update_status,
-            "update_exception": update_exception,
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "feed_config.html", {"request": request, **response}
+        )
 
 
 @app.get("/feeds/new/", response_class=HTMLResponse)
 async def new_feed(request: Request, update_exception: str = None):
+    response = {
+        "settings": await bk.get_settings(),
+        "feed": {},
+        "update_exception": update_exception,
+    }
 
-    return templates.TemplateResponse(
-        "feed_config.html",
-        {
-            "request": request,
-            "settings": await bk.get_settings(),
-            "feed": {},
-            "update_exception": update_exception,
-        },
-    )
+    if request.headers.get("accept") == JSON:
+        return JSONResponse(content=response, media_type=JSON)
+    else:
+        return templates.TemplateResponse(
+            "feed_config.html", {"request": request, **response}
+        )
 
 
 # Utility APIs - mostly used to orchestrate tests, but available for whatever
