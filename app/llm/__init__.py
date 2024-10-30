7e205bbd9e66509ff982ecbb8f27fe63bc3a2110
Leo Qin <leozqin@gmail.com>
2024-10-29 19:04:44 -0700

    feat: redirect null summarization handler to null llm

--- app/llm/__init__.py
+++ app/llm/__init__.py
@@ -1,11 +1,12 @@
-from enum import Enum
-
 from app.llm.null import NullLLMHandler
 from app.llm.ollama import OllamaLLMHandler
 from app.llm.openai import OpenAILLMHandler
 
 llm_handlers = {
-    "null_llm": NullLLMHandler,
-    "ollama": OllamaLLMHandler,
-    "openai": OpenAILLMHandler,
+    NullLLMHandler.id: NullLLMHandler,
+    OllamaLLMHandler.id: OllamaLLMHandler,
+    OpenAILLMHandler.id: OpenAILLMHandler,
+    # redirect null summarization handler to null llm
+    # TODO: Deprecate
+    "null_summarization": NullLLMHandler,
 }
